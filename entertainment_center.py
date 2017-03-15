4ef5c0c0dd73fc7a4c5121ea753239052ada6134
StefanM <stefan.fva@gmail.com>
2017-03-15 19:03:05 -0400

    Altered code to meet PEP 8 Style

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,27 +3,27 @@ import fresh_tomatoes
 
 #  Create new instances of the Movie class for each movie
 moana = media.Movie("Moana",
-                    "https://upload.wikimedia.org/wikipedia/en/2/26/" \
+                    "https://upload.wikimedia.org/wikipedia/en/2/26/"
                     "Moana_Teaser_Poster.jpg",
                     "https://youtu.be/LKFuXETZUsI")
 
 zootopia = media.Movie("Zootopia",
-                       "https://upload.wikimedia.org/wikipedia/en/thumb/" \
+                       "https://upload.wikimedia.org/wikipedia/en/thumb/"
                        "e/ea/Zootopia.jpg/220px-Zootopia.jpg",
                        "https://youtu.be/jWM0ct-OLsM")
 
 deadpool = media.Movie("Deadpool",
-                       "https://upload.wikimedia.org/wikipedia/en/4/46/" \
+                       "https://upload.wikimedia.org/wikipedia/en/4/46/"
                        "Deadpool_poster.jpg",
                        "https://youtu.be/ONHBaC-pfsk")
 dumb_and_dumber = media.Movie("Dumb and Dumber",
-                              "https://images-na.ssl-images-amazon.com/" \
+                              "https://images-na.ssl-images-amazon.com/"
                               "images/I/51XVZYDA0ZL.jpg",
                               "https://youtu.be/l13yPhimE3o")
 walle = media.Movie("WALL-E",
-                     "http://www.gstatic.com/tv/thumb/movieposters/" \
-                     "174374/p174374_p_v8_ab.jpg",
-                     "https://youtu.be/vbLNOFbsRow")
+                    "http://www.gstatic.com/tv/thumb/movieposters/"
+                    "174374/p174374_p_v8_ab.jpg",
+                    "https://youtu.be/vbLNOFbsRow")
 
 #  Place all our movies into an array
 movies = [moana, zootopia, deadpool, dumb_and_dumber, walle]
